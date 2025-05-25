156cffe47dd7f04f824c02b9b37b941e195f30b3
Ikhsan Syahri Ramadhan <masari311003@gmail.com>
2025-05-25 23:39:24 +0700

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-service-122089089856.us-central1.run.app/api',
+  baseURL: 'https://backend-projectakhir-122089089856.us-central1.run.app/api',
 });
 
 export default api;
