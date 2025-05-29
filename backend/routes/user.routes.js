7fdb9362fa734dd283454badff3daa1cf844044e
Ikhsan Syahri Ramadhan <masari311003@gmail.com>
2025-05-30 01:18:28 +0700

    buatmobile

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { register, login, getMe } from '../controllers/user.controller.js';
-import verifyToken from '../middleware/verifyToken.js';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
